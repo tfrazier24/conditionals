c2271d41f8cbb62f1a861742014f0623ae818c75
Thomas <tfrazier2424@gmail.com>
2020-12-10 17:55:29 -0500

    working on conditionals exercises

---
+++ script.js
@@ -0,0 +1,61 @@
+let likesToTravel = true
+
+if(likesToTravel === true){
+    console.log("Bon voyage!")
+}
+else{
+    console.log("Enjoy your couch")
+}
+
+let favoriteFood = "thai"
+
+if(favoriteFood === "fried pickles" || favoriteFood === "pepperoni rolls" ){
+    console.log("Congratulations! You have excellent taste!")
+}
+else{
+    console.log("Clearly you have not tried fried pickles or pepperoni rolls")
+}
+
+if(1 + 1 >5){
+    console.log("True fact!")
+}
+else{
+    console.log("LIES!")
+}
+
+if(4 + 5 <=20){
+    console.log("True fact!")
+}
+else{
+    console.log("LIES!")
+}
+
+if(6 - 2 >=0){
+    console.log("True fact!")
+}
+else{
+    console.log("LIES!")
+}
+
+let likesDogs = true
+
+if(likesDogs === true ){
+    console.log("You're a dog person!")
+}
+else{
+    console.log("You're a cat person!")
+}
+
+let birthYear = 1945
+
+if(birthYear === 1996){
+    console.log("You must be one of them Gen Z kids")
+}
+else{
+    console.log("Don't lie to young fella")
+}
+
+let nameObject = {
+    personName: "Thomas",
+    preferredLanguage: "English",
+}
